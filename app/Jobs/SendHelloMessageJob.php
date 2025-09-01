91451c6217b6c09e2b40f3883d92be16ae6fb4a8
Aman Vaghela <vaghelaamankumar@gmail.com>
2025-09-01 10:55:12 +0530

    code dependancy added

--- app/Jobs/SendHelloMessageJob.php
+++ app/Jobs/SendHelloMessageJob.php
@@ -6,13 +6,15 @@ use App\Models\User;
 use Illuminate\Bus\Queueable;
 use App\Mail\DailyMessageMail;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue; 
-use Illuminate\Foundation\Events\Dispatchable;
+// use Illuminate\Foundation\Events\Dispatchable;
 
 class SendHelloMessageJob implements ShouldQueue 
 {
-    use Queueable, SerializesModels, Dispatchable;
+    use Queueable, SerializesModels, Dispatchable , InteractsWithQueue;
 
     public $message;
 
